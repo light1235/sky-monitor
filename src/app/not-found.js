9fb99be2135c8e371305290564ee5a418b15ccef
Lighty <shliakhov.frontend@gmail.com>
2024-03-14 13:52:37 +0200

    add 404 style

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './index.scss'
 
 const notFound = () => {
      return (
-          <div>
-               soaodaodaodoad
+          <div className="error-page">
+               <div className="error-page-container">
+                    <div className="error-page-content">
+                         <h1>404</h1>
+                         <h2>Something  went
+                              wrong =(
+                         </h2>
+                    </div>
+               </div>
           </div>
      );
 };
