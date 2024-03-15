1ad344d1e3c1d1f329931a5cf62328549f19e7ae
Lighty <shliakhov.frontend@gmail.com>
2024-03-15 19:22:22 +0200

    change text

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -20,8 +20,8 @@ const Services = () => {
                               <div className="logo-circle"><Image src={listingLogo} width='40' alt={'listing icon'}/></div>
                          </div>
                          <div className="item-title">Listing Advertising</div>
-                         <div className="item-description">description of the listig packages and what they include
-                              {/*описание пакетов услуг для листига и что в них входит*/}
+                         <div className="item-description">
+                              описание пакетов услуг для листига и что в них входит
                          </div>
                          <div className="item-link"><Link href='/listing-advertising'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
@@ -31,8 +31,8 @@ const Services = () => {
                               <div className="logo-circle"><Image src={bannerLogo}  width='40'alt={'listing icon'}/></div>
                          </div>
                          <div className="item-title">Banner Advertising</div>
-                         <div className="item-description">detailed description of the banner service you can receive
-                              {/*детальное описание услуги баннера которую вы можете получить*/}
+                         <div className="item-description">
+                              детальное описание услуги баннера которую вы можете получить
                          </div>
                          <div className="item-link"><Link href='/'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
@@ -41,9 +41,10 @@ const Services = () => {
                          <div className="item-logo">
                               <div className="logo-circle"><Image src={popLogo} width='40' alt={'listing icon'}/></div>
                          </div>
+
                          <div className="item-title">Pop-up Advertising</div>
-                         <div className="item-description">what the pop-up service looks like and how it works
-                              {/*как выглядит услуга поп ап и как она работает*/}
+                         <div className="item-description">
+                              как выглядит услуга поп ап и как она работает-
                          </div>
                          <div className="item-link"><Link href='/'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
@@ -53,8 +54,8 @@ const Services = () => {
                               <div className="logo-circle"><Image src={listingPin} width='40' alt={'listing icon'}/></div>
                          </div>
                          <div className="item-title">Listing pin to top</div>
-                         <div className="item-description">description of the service itself and its advantages
-                              {/*описание самой услуги а так же ее приемуществ*/}
+                         <div className="item-description">
+                              описание самой услуги а так же ее приемуществ
                          </div>
                          <div className="item-link"><Link href='/'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
@@ -64,8 +65,8 @@ const Services = () => {
                               <div className="logo-circle"><Image src={listingRaise} width='40' alt={'listing icon'}/></div>
                          </div>
                          <div className="item-title">Listing raise to top</div>
-                         <div className="item-description">additional service for current advertisers
-                              {/*дополнительная услуга для актуальных рекламодателей*/}
+                         <div className="item-description">
+                              дополнительная услуга для актуальных рекламодателей
                          </div>
                          <div className="item-link"><Link href='/'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
@@ -75,8 +76,8 @@ const Services = () => {
                               <div className="logo-circle"><Image src={projectLogo} width="40" alt={'listing icon'}/></div>
                          </div>
                          <div className="item-title">Project pin to top</div>
-                         <div className="item-description">description of the project description service
-                              {/*описание услуги  описания проекта*/}
+                         <div className="item-description">
+                              описание услуги  описания проекта
                          </div>
                          <div className="item-link"><Link href='/'><Image src={linkLogo} alt='link-icon'></Image></Link>
                          </div>
