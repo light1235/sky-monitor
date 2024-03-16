26b399faa610f63782f8c98701e9058b536b47b3
Lighty <shliakhov.frontend@gmail.com>
2024-03-16 18:20:24 +0200

    change path

--- src/components/change-navigation/index.js
+++ src/components/change-navigation/index.js
@@ -10,8 +10,7 @@ import ControlMenu from "@/components/panel/control_footer";
 const ChangeNavigation = ({children}) => {
 
      const pathname = usePathname();
-     const isCustomHeaderFooterPage = pathname === '/panel'|| 'panel/seo-statistics';
-
+     const isCustomHeaderFooterPage = pathname === '/panel' || pathname === '/panel/seo-statistics';
 
      return (
           <>
