338c66e227f4feb66b4935cb9c855092a0d7617c
Lighty <shliakhov.frontend@gmail.com>
2024-03-29 18:47:44 +0200

    add payments system logos and logics -max -6

--- src/components/main/listing_card_mobile/index.js
+++ src/components/main/listing_card_mobile/index.js
@@ -7,6 +7,17 @@ import perfectMoneyLogo from "@/assets/main/icons/payments-system/perfect-money.
 import netellerLogo from "@/assets/main/icons/payments-system/neteller.svg";
 import qiwiLogo from "@/assets/main/icons/payments-system/qiwi.svg";
 import webMoney from "@/assets/main/icons/payments-system/web-money.svg";
+import bnb from '/src/assets/main/icons/payments-system/bnb.svg';
+import dodgeCoin from '/src/assets/main/icons/payments-system/dodge-coin.svg';
+import eth from '/src/assets/main/icons/payments-system/eth.svg';
+import liteCoin  from  '/src/assets/main/icons/payments-system/lite-coin.svg';
+import payoneer from '/src/assets/main/icons/payments-system/payoneer.svg';
+import payPall from '/src/assets/main/icons/payments-system/pay-pall.svg';
+import payza from '/src/assets/main/icons/payments-system/payza.svg';
+import ripple from '/src/assets/main/icons/payments-system/ripple.svg';
+import skrill from '/src/assets/main/icons/payments-system/skrill.svg';
+import tether from '/src/assets/main/icons/payments-system/tether.svg';
+import tron from '/src/assets/main/icons/payments-system/tron-trx.svg';
 import Link from "next/link";
 import CustomToolTop from "@/components/main/tooltip_custom";
 import simillarWeb from "@/assets/main/icons/simillar-web.svg";
@@ -44,6 +55,12 @@ const MobileListing = ({ item}) => {
                setTruncatedText(text);
           }
      }, []);
+
+
+     const paymentSystems = Object.keys(item.paymentSystem).filter(system => item.paymentSystem[system]);
+
+     // Ограничение длины массива до 6 элементов
+     const displayedPaymentSystems = paymentSystems.slice(0, 6);
 
      return (
           <div className={item.projectInformation.status.scam ? "listing__card-mobile scam-status" : 'listing__card-mobile'}>
@@ -90,12 +107,27 @@ const MobileListing = ({ item}) => {
                     <div className="right_side">
                     <div className="description">Accepted:</div>
                          <div className="payments-system">
-                              {item.paymentSystem.adv && <Image src={advLogo} height="15" width="15" alt="payment-system-logo"/>}
-                              {item.paymentSystem.bitcoin && <Image src={bitcoinLogo} height="15" width="15" alt="payment-system-logo" />}
-                              {item.paymentSystem.perfectMoney && <Image src={perfectMoneyLogo} height="15" width="15" alt="payment-system-logo" />}
-                              {item.paymentSystem.neteller && <Image src={netellerLogo} height="15" width="15" alt="payment-system-logo"/>}
-                              {item.paymentSystem.qiwi && <Image src={qiwiLogo} height="15" width="15" alt="payment-system-logo"/>}
-                              {item.paymentSystem.webMoney && <Image src={webMoney} height="15" width="15" alt="payment-system-logo"/>}
+                              {displayedPaymentSystems.map(system => (
+                                   <div key={system} className="payment-system-icon">
+                                        {system === "bitcoin" && <Image src={bitcoinLogo} height="15" width="15" alt="Bitcoin Logo" />}
+                                        {system === "neteller" && <Image src={netellerLogo} height="15" width="15" alt="Neteller Logo" />}
+                                        {system === "qiwi" && <Image src={qiwiLogo} height="15" width="15" alt="Qiwi Logo" />}
+                                        {system === "webMoney" && <Image src={webMoney} height="15" width="15" alt="WebMoney Logo" />}
+                                        {system === "perfectMoney" && <Image src={perfectMoneyLogo} height="15" width="15" alt="Perfect Money Logo" />}
+                                        {system === "adv" && <Image src={advLogo} height="15" width="15" alt="Adv Logo" />}
+                                        {system === "bnb" && <Image src={bnb} height="15" width="15" alt="Binance Logo" />}
+                                        {system === "dodgeCoin" && <Image src={dodgeCoin} height="15" width="15" alt="DodgeCoin Logo" />}
+                                        {system === "eth" && <Image src={eth} height="15" width="15" alt="Ethereum Logo" />}
+                                        {system === "liteCoin" && <Image src={liteCoin} height="15" width="15" alt="LiteCoin Logo" />}
+                                        {system === "payoneer" && <Image src={payoneer} height="15" width="15" alt="Payoneer Logo" />}
+                                        {system === "payPall" && <Image src={payPall} height="15" width="15" alt="PayPal Logo" />}
+                                        {system === "payza" && <Image src={payza} height="15" width="15" alt="Payza Logo" />}
+                                        {system === "ripple" && <Image src={ripple} height="15" width="15" alt="Ripple Logo" />}
+                                        {system === "skrill" && <Image src={skrill} height="15" width="15" alt="Skrill Logo" />}
+                                        {system === "tether" && <Image src={tether} height="15" width="15" alt="Tether Logo" />}
+                                        {system === "tron" && <Image src={tron} height="15" width="15" alt="Tron Logo" />}
+                                   </div>
+                              ))}
                          </div>
                          <div className="info">Info</div>
                          <div className="project-info">
