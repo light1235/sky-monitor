b3aeb8ceec2ad210a997beed4856433eacb610c4
Lighty <shliakhov.frontend@gmail.com>
2024-03-29 17:54:53 +0200

    add final banner logic

--- src/components/main/content-banner/index.js
+++ src/components/main/content-banner/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const ContentBanner = ({image}) => {
+
      return (
           <>
                <Link target="_blank" href="https://wizerpeople.com/"> <Image width="615" height="90" src={image} alt='banner-image' /></Link>
--- src/components/main/content-block/index.js
+++ src/components/main/content-block/index.js
@@ -2,8 +2,6 @@
 import React, {useEffect, useState} from 'react';
 import Listing_card from "@/components/main/listing_card";
 import './index.scss'
-import Image from "next/image";
-import Link from "next/link";
 import Listing_card_mobile from "@/components/main/listing_card_mobile";
 import ContentBanner from "@/components/main/content-banner";
 
@@ -11,49 +9,34 @@ import ContentBanner from "@/components/main/content-banner";
 
 const ContentBlock = ({items}) => {
 
-     const BannersData = [
-          { id: 1, banners: ['https://cdn.discordapp.com/attachments/1046398987659395152/1222300614822858975/lapaido728e.gif?ex=6615b727&is=66034227&hm=ea9ab7c7d1c08748dcbafddd521bb9847edd1996174f957e9bff84bc98d76c6a&', 'https://cdn.discordapp.com/attachments/1046398987659395152/1222301455751450727/hyiproom468ee.gif?ex=6615b7f0&is=660342f0&hm=08e151b28c710eda72999109636b1831e8100e76e633f505b2f1a6a9d2fd7063&'] },
-          { id: 2, banners: ['https://cdn.discordapp.com/attachments/1046398987659395152/1223050673155866684/489.gif?ex=661871b3&is=6605fcb3&hm=1f1cf894e284f09ae4e70760823075a2e9b54042294dc3205e9f78e2d18c1599&', 'https://cdn.discordapp.com/attachments/1046398987659395152/1223050746887405609/493.gif?ex=661871c5&is=6605fcc5&hm=28e0589eba23113b67b0a94d437743fa91eb7ff772807aa938fe499481ccee84&'] },
-          { id: 3, banners: ['https://cdn.discordapp.com/attachments/1046398987659395152/1223050710527115386/492.gif?ex=661871bc&is=6605fcbc&hm=014f81456a832e02ce2666106a28bde42707f2bb7ea674d33cada22297623db0&', 'https://cdn.discordapp.com/attachments/1046398987659395152/1223050593866875002/rotor.webp?ex=661871a0&is=6605fca0&hm=901e0330e249d9f902ad2497d5df394e28889f21784e1ddd89f2bf944c3e183f&'] },
-     ];
+     const RandomArr = ['https://cdn.discordapp.com/attachments/1046398987659395152/1222300614822858975/lapaido728e.gif?ex=6615b727&is=66034227&hm=ea9ab7c7d1c08748dcbafddd521bb9847edd1996174f957e9bff84bc98d76c6a&','https://cdn.discordapp.com/attachments/1046398987659395152/1222301455751450727/hyiproom468ee.gif?ex=6615b7f0&is=660342f0&hm=08e151b28c710eda72999109636b1831e8100e76e633f505b2f1a6a9d2fd7063&','https://cdn.discordapp.com/attachments/1046398987659395152/1223050673155866684/489.gif?ex=661871b3&is=6605fcb3&hm=1f1cf894e284f09ae4e70760823075a2e9b54042294dc3205e9f78e2d18c1599&','https://cdn.discordapp.com/attachments/1046398987659395152/1223050746887405609/493.gif?ex=661871c5&is=6605fcc5&hm=28e0589eba23113b67b0a94d437743fa91eb7ff772807aa938fe499481ccee84&','https://cdn.discordapp.com/attachments/1046398987659395152/1223050710527115386/492.gif?ex=661871bc&is=6605fcbc&hm=014f81456a832e02ce2666106a28bde42707f2bb7ea674d33cada22297623db0&','https://cdn.discordapp.com/attachments/1046398987659395152/1223050593866875002/rotor.webp?ex=661871a0&is=6605fca0&hm=901e0330e249d9f902ad2497d5df394e28889f21784e1ddd89f2bf944c3e183f&'];
+
+
+     const BannersData = RandomArr.map((random, index) => ({
+          id: index + 1,
+          banners: [RandomArr[Math.floor(Math.random() * RandomArr.length)], RandomArr[Math.floor(Math.random() * RandomArr.length)]],
+     }));
 
      const [blocks, setBlocks] = useState(BannersData);
 
 
      useEffect(() => {
           const intervalId = setInterval(() => {
-               shuffleBlocks();
+               // shuffleBlocks();
+               updateBlocks();
           }, 6000);
 
           return () => clearInterval(intervalId);
      }, []);
 
-     // const shuffleBlocks = () => {
-     //      const newBlocks = [...BannersData];
-     //      newBlocks.forEach(block => {
-     //           block.banners = shuffleArray(block.banners);
-     //      });
-     //      setBlocks(shuffleArray(newBlocks));
-     // };
-     //
-     // const shuffleArray = (array) => {
-     //      const newArray = [...array];
-     //      for (let i = newArray.length - 1; i > 0; i--) {
-     //           const j = Math.floor(Math.random() * (i + 1));
-     //           [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-     //      }
-     //      return newArray;
-     // };
 
-     const shuffleBlocks = () => {
-          const newBlocks = [...blocks];
-          for (let i = newBlocks.length - 1; i > 0; i--) {
-               const j = Math.floor(Math.random() * (i + 1));
-               [newBlocks[i], newBlocks[j]] = [newBlocks[j], newBlocks[i]];
-          }
+     const updateBlocks = () => {
+          const newBlocks = blocks.map(block => ({
+               id: block.id,
+               banners: [RandomArr[Math.floor(Math.random() * RandomArr.length)], RandomArr[Math.floor(Math.random() * RandomArr.length)]],
+          }));
           setBlocks(newBlocks);
      };
-
 
 
      const [windowWidth, setWindowWidth] = useState(1420);
