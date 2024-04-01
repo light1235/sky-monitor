1d45d32cee289fe0241f1fdec4b2cb7ee66b0678
Lighty <shliakhov.frontend@gmail.com>
2024-04-01 22:20:33 +0300

    add final logo

--- src/components/main/header/index.js
+++ src/components/main/header/index.js
@@ -2,7 +2,7 @@
 import React, {useEffect, useState} from 'react';
 import './index.scss'
 import Image from 'next/image'
-import LOGO from '/src/assets/logo-5.svg'
+import LOGO from '/src/assets/logo-7.svg'
 import Link from "next/link";
 import anime from 'animejs';
 import monIco from '/src/assets/main/icons/monitor.svg';
