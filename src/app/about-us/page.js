bd82e6a43d60cc0444137858ff0c21b9a128b3a5
Lighty <shliakhov.frontend@gmail.com>
2024-04-28 21:53:47 +0300

    some changes

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss'
 import Image from "next/image";
-import AboutImage from '../../assets/main/images/news.jpg'
+import AboutImage from '../../assets/main/images/about-us-3.jpg'
 
 const AboutUs = () => {
      return (
