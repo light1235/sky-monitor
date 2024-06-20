5d940e1815a166534524e7a060383e45b28744ea
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 00:55:03 +0300

    change onclick last

--- src/components/panel/custom-button/index.js
+++ src/components/panel/custom-button/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Button} from "antd";
 
-const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%', click = () => {console.log("123")} }) => {
+const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%', click = () => {}}) => {
+
+     const handleClick = () => {
+          click()
+     }
+
+
      return (
           <>
-               <Button type={type}  htmlType="submit" size={'large'} onClick={click} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
+               <Button type={type}  htmlType="submit" size={'large'} onClick={handleClick} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
                     {name}
                </Button>
           </>
