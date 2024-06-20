ab8a23aa9fc73afbc3e396eaa4ecc7cba7c5434e
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 00:42:19 +0300

    change onclick

--- src/components/panel/custom-button/index.js
+++ src/components/panel/custom-button/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Button} from "antd";
 
-const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%',click = () => {} }) => {
+const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%', click = () => {} }) => {
      return (
           <>
-               <Button type={type}  htmlType="submit" size={'large'} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
+               <Button type={type}  htmlType="submit" size={'large'} onClick={click} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
                     {name}
                </Button>
           </>
