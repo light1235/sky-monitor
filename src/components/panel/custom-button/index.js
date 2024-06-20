8731f4e9faf140fc111cec0a979561d4175451ee
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 00:44:42 +0300

    change onclick and log

--- src/components/panel/custom-button/index.js
+++ src/components/panel/custom-button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button} from "antd";
 
-const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%', click = () => {} }) => {
+const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%', click = () => {console.log("123")} }) => {
      return (
           <>
                <Button type={type}  htmlType="submit" size={'large'} onClick={click} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
