8597bd91a3188220818367fd17afda6f22ef942b
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 00:34:03 +0300

    change button

--- src/components/panel/custom-button/index.js
+++ src/components/panel/custom-button/index.js
@@ -4,7 +4,7 @@ import {Button} from "antd";
 const CustomButton = ({name = 'Add new', type= 'primary', color= '#85CE36', width = '100%',click = () => {} }) => {
      return (
           <>
-               <Button type={type} onClick={click} htmlType="submit" size={'large'} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
+               <Button type={type}  htmlType="submit" size={'large'} style={{width:`${width}`, background:`${color}`,fontFamily:'Open Sans',fontWeight:'600',padding:'0 20px 0 20px'}}>
                     {name}
                </Button>
           </>
