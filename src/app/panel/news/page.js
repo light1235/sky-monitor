5da3fcc49a6aed57a84a1717fc317f239f90c6ce
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 12:59:36 +0300

    add news 4

--- src/app/panel/news/page.js
+++ src/app/panel/news/page.js
@@ -8,6 +8,7 @@ import OptionTableLine from "@/components/panel/option-table-line";
 import TableLine from "@/components/panel/standard-table-line";
 import CustomPagination from "@/components/panel/pagination";
 import Custom_modal from "@/components/main/custom_modal";
+import NewsStepper from "@/components/panel/news-stepper";
 
 
 const items = [
@@ -109,7 +110,8 @@ const NewsPage = () => {
                     <CustomPagination/>
                </div>
                <Custom_modal open={activeModal} close={handleModal}>
-                    777777
+
+                    <NewsStepper />
                </Custom_modal>
           </div>
      );
