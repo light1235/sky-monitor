ecbefced9097b655c7c159ea80bb7d3b73705de3
Lighty <shliakhov.frontend@gmail.com>
2024-06-21 12:57:05 +0300

    add news 3

--- src/app/panel/support/page.js
+++ src/app/panel/support/page.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './index.scss'
+import NewsStepper from "@/components/panel/news-stepper";
 
 
 const SupportPage = () => {
      return (
           <div className="support__page">
-               {/*<NewsStepper />*/}
-              {/*<AddNewsForm />*/}
-
+               <NewsStepper />
           </div>
      );
 };
