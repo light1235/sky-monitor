caf7a67664299fadc4867e854d3e13112cf35739
Lighty <shliakhov.frontend@gmail.com>
2024-07-31 01:29:33 +0300

    underline and without link

--- src/components/main/information_line/index.js
+++ src/components/main/information_line/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import './index.scss';
 import Link from "next/link";
 
-const InformationLine = ({ image,items,scam}) => {
+const InformationLine = ({ image, items, scam }) => {
      const domainStrings = [];
      if (items && items.name) {
-               domainStrings.push(items.name);
+          domainStrings.push(items.name);
      }
 
      const removeDomain = /\..*$/;
@@ -16,50 +16,25 @@ const InformationLine = ({ image,items,scam}) => {
                .replace(/-/g, '\u2011')
      );
 
+     const content = (
+          <div className="information-line">
+               <div className="line-items">
+                    <div className="item-logo">{image}</div>
+                    <div className="item-name">{items && modifiedStrings}</div>
+               </div>
+               <div className="line-items-one">
+                    {scam ? <div>Warning</div> :
+                         <div className="item-numbers" style={{textDecoration:'none'}}>{items && items.projectInformation.created}</div>
+                    }
+               </div>
+          </div>
+     );
+
      return (
           <>
-               <Link target="_blank" href='/' className="information-line">
-                    <div className="line-items">
-                         <div className="item-logo">{image}</div>
-                         <div className="item-name">{items && modifiedStrings}</div>
-                    </div>
-                    <div className="line-items-one">
-                         {scam ? <div>Warning</div> :
-                              <div className="item-numbers">{items && items.projectInformation.created}</div>
-                         }
-
-                    </div>
-               </Link>
+               {scam ? content : <Link target="_blank" href='/' style={{textDecoration:'none'}}>{content}</Link>}
           </>
      );
 };
 
 export default InformationLine;
-
-
-
-
-
-// const YourComponent = () => {
-//      // Исходные строки с разными доменами
-//      const domainStrings = ['Asignat.com', 'AnotherSite.ru', 'Example.org', 'MySite.me'];
-//
-//      // Регулярное выражение для удаления доменов
-//      const removeDomain = /\..*$/;
-//
-//      // Удаление доменов из строк
-//      const modifiedStrings = domainStrings.map((originalString) =>
-//           originalString.replace(removeDomain, '')
-//      );
-//
-//      return (
-//           <div>
-//                {/* Вывод измененных строк без доменов */}
-//                {modifiedStrings.map((modifiedString, index) => (
-//                     <div key={index}>{modifiedString}</div>
-//                ))}
-//           </div>
-//      );
-// };
-//
-// export default YourComponent;
