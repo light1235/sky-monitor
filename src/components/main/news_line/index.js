7858a40eaf625b8c137c84a4ed27755c48374d41
Lighty <shliakhov.frontend@gmail.com>
2024-07-31 01:16:20 +0300

    change data12

--- src/components/main/news_line/index.js
+++ src/components/main/news_line/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const NewsLine = ({image,item}) => {
      return (
-          <Link target="_blank" href={`/projects/${item.slug.replace(/\s+/g, '-')}`} className="news-information-line">
+          <Link  href={`/projects/${item.slug.replace(/\s+/g, '-')}`} className="news-information-line">
                <div className="line-items">
                     <div className="item-logo">{image}</div>
                     <div className="item-name">{item.preview.description}</div>
