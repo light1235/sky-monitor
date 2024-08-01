9947b521d6c6077290631b447e4a944ef7af3f16
Lighty <shliakhov.frontend@gmail.com>
2024-08-01 12:31:54 +0300

    test query speed in server component delete

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,28 +1,27 @@
+"use client"
 import React  from 'react';
 import './index.scss';
 import Project_preview from "@/components/main/project_preview";
 import {useGetProjectQuery} from "@/services/projectApi";
-import ProjectWrap from "@/components/main/project-wrap";
 
 const Projects = () => {
 
-     // const { data : projects = [], error: commentsError, isLoading: commentsLoading } = useGetProjectQuery(undefined, {
-     //      pollingInterval: 60000,
-     //      refetchOnMountOrArgChange: true,
-     //      refetchOnReconnect: true,
-     // });
-     //
-     // const projectItem = [...projects].reverse();
+     const { data : projects = [], error: commentsError, isLoading: commentsLoading } = useGetProjectQuery(undefined, {
+          pollingInterval: 60000,
+          refetchOnMountOrArgChange: true,
+          refetchOnReconnect: true,
+     });
+
+     const projectItem = [...projects].reverse();
 
 
      return (
           <section className="projects__page">
                <div className="projects__page-container">
                     <div className="projects__page-content">
-                         {/*{projectItem.map((post, index) =>*/}
-                         {/*     <Project_preview key={post.id} item={post} link={`/projects/${post.slug.replace(/\s+/g, '-')}`} />*/}
-                         {/*)}*/}
-                         <ProjectWrap />
+                         {projectItem.map((post, index) =>
+                              <Project_preview key={post.id} item={post} link={`/projects/${post.slug.replace(/\s+/g, '-')}`} />
+                         )}
 
                     </div>
                </div>
