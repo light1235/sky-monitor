18b70f191a27c52620dac4548afed264e6ab20fd
Lighty <shliakhov.frontend@gmail.com>
2024-09-03 23:03:03 +0300

    add article data

--- src/components/main/news_line/index.js
+++ src/components/main/news_line/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './index.scss'
 import {Link} from '@/i18n/routing.public'
+import { useLocale } from 'next-intl';
 
 const NewsLine = ({image,item}) => {
+
+     const locale = useLocale();
      return (
           <Link   href={`/projects/${item.slug.replace(/\s+/g, '-')}`} className="news-information-line">
                <div className="line-items">
                     <div className="item-logo">{image}</div>
-                    <div className="item-name">{item.preview.description}</div>
+                    <div className="item-name">{locale === "en" ? item.preview.description : item.preview.descriptionRU}</div>
                </div>
           </Link>
      );
