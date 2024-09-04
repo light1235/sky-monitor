2f4245a0a6a53f45d5741c6b128acaf3591f637e
Lighty <shliakhov.frontend@gmail.com>
2024-09-04 13:49:02 +0300

    add login translate

--- src/app/[locale]/login/page.js
+++ src/app/[locale]/login/page.js
@@ -7,19 +7,26 @@ import CustomFrontButton from "@/components/main/cutom_front_button";
 import CustomFrontInput from "@/components/main/custom-front-input";
 import {Link} from '@/i18n/routing.public'
 import CustomCheckBox from "@/components/panel/check-box";
+import {useTranslations} from 'next-intl';
+import {useLocale} from 'next-intl';
 
-const validationSchema = Yup.object().shape({
-     email: Yup.string().email('Invalid email address').required('Email is required'),
-     password: Yup.string().min(6, 'Password must be at least 6 characters long').required('Password is required'),
-});
+
+
 
 const LoginPage = () => {
+     const t = useTranslations();
+     const locale = useLocale();
      const [isValidLogin, setIsValidLogin] = useState(false);
 
      const [formData, setFormData] = useState({
           email: '',
           password: '',
           forgot:'false',
+     });
+
+     const validationSchema = Yup.object().shape({
+          email: Yup.string().email(locale === 'en' ?'Invalid email address':'Неверный адрес электронной почты').required(locale === 'en' ?'Email is required':'Электронная почта обязательна'),
+          password: Yup.string().min(6, locale === 'en' ?'Password must be at least 6 characters long':'Пароль должен состоять не менее чем из 6 символов').required(locale === 'en' ?'Password is required':'Пароль обязателен'),
      });
 
      useEffect(() => {
@@ -39,8 +46,8 @@ const LoginPage = () => {
                <div className="login__content">
                     <div className="content__form">
                          <div className="form-container">
-                              <h1>Log in</h1>
-                              <h2>Dive into the abyss!</h2>
+                              <h1>{t('Register.title')}</h1>
+                              <h2>{t('Register.description')}</h2>
                               <Formik
                                    initialValues={{ email: '', password: ''}}
                                    validationSchema={validationSchema}
@@ -60,21 +67,21 @@ const LoginPage = () => {
                                    {({ isSubmitting }) => (
                                         <Form>
                                              <label>
-                                                  <p>Email*</p>
+                                                  <p>{t('Register.email')}</p>
                                                   <Field as={CustomFrontInput} type="email" name="email"/>
                                                   <ErrorMessage name="email" component="div" className="inputError"/>
                                              </label>
                                              <label>
-                                                  <p>Password*</p>
+                                                  <p>{t('Register.password')}</p>
                                                   <Field as={CustomFrontInput} type="password" name="password"/>
                                                   <ErrorMessage name="password" component="div" className="inputError"/>
                                              </label>
-                                             <CustomFrontButton disblad={isSubmitting} name={'Get Started'}/>
+                                             <CustomFrontButton disblad={isSubmitting} name={t('Register.button')}/>
                                              <label className="login-check">
-                                                  <CustomCheckBox onSelect={handleForgotChange}  />Remember me
+                                                  <CustomCheckBox onSelect={handleForgotChange}  />{t('Register.remember')}
                                              </label>
                                              <div className="form-separator"></div>
-                                             <div className="ps-reset"><Link href="/reset">Forgot your password ?</Link></div>
+                                             <div className="ps-reset"><Link href="/reset">{t('Register.link')}</Link></div>
                                         </Form>
                                    )}
                               </Formik>
