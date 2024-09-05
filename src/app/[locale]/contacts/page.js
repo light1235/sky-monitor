2bb8e63f8d2e802589c51d81d2e469bf0a079992
Lighty <shliakhov.frontend@gmail.com>
2024-09-05 18:17:05 +0300

    add meta data on contacts

--- src/app/[locale]/contacts/page.js
+++ src/app/[locale]/contacts/page.js
@@ -4,10 +4,29 @@ import Image from "next/image";
 import contactPageImage from '/src/assets/main/images/contanct-page.svg'
 import {useTranslations} from 'next-intl';
 
-export const metadata = {
-     title: 'Sky Finance - Expert in investments',
-     description: 'Reach out to us for personalized financial guidance and assistance',
-};
+// export const metadata = {
+//      title: 'Sky Finance - Expert in investments',
+//      description: 'Reach out to us for personalized financial guidance and assistance',
+// };
+export async function generateMetadata({ params }) {
+     const locale = params?.locale || 'en';
+
+     const title = locale === 'ru' ? 'Добро пожаловать на наш сайт' : 'Sky Finance - Expert in investments';
+     const description = locale === 'ru' ? 'Это главная страница на русском языке' : 'Reach out to us for personalized financial guidance and assistance';
+
+     return {
+          title,
+          description,
+          alternates: {
+               canonical: locale === 'ru' ? 'https://example.com/ru' : 'https://example.com/en',
+               languages: {
+                    'ru': 'https://example.com/ru',
+                    'en': 'https://example.com/en',
+               },
+          },
+     };
+}
+
 
 const Contacts = () => {
      const t = useTranslations();
