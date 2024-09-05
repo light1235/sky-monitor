8584017d61fa0baa5b59b010fdbb015b9fa8ed83
Lighty <shliakhov.frontend@gmail.com>
2024-09-05 13:02:13 +0300

    add privacy page translate

--- src/app/[locale]/privacy-policy/page.js
+++ src/app/[locale]/privacy-policy/page.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './index.scss'
+import {useTranslations} from 'next-intl';
+
 
 const PrivacyPolicy = () => {
+     const t = useTranslations();
      return (
           <section className="policy__page">
                <div className="page-container">
-               <h1>Protecting your privates: Our coverage</h1>
-               <h2>We care approximately your privates</h2>
-               <h3>Information Collection</h3>
-               <p>We acquire data about our users for we to operate our platform and enhance the consumer experience. This consists of information along with e mail addresses.</p>
-               <h3>Use of Information</h3>
-               <p>The records collected is used totally to improve the platform and user revel in. For instance, we might also use this statistics to investigate pastime on the web site and customize services.</p>
-               <h3>Storage of Information</h3>
-               <p>We are dedicated to storing consumer records securely and according with applicable data protection rules. This includes taking measures to protect against unauthorized access and information leaks.
+               <h1>{t('PrivacyPolicy.h1')}</h1>
+               <h2>{t('PrivacyPolicy.h2')}</h2>
+               <h3>{t('PrivacyPolicy.title')}</h3>
+               <p>{t('PrivacyPolicy.description')}</p>
+               <h3>{t('PrivacyPolicy.title1')}</h3>
+               <p>{t('PrivacyPolicy.description1')}</p>
+               <h3>{t('PrivacyPolicy.title2')}</h3>
+               <p>{t('PrivacyPolicy.description2')}
                </p>
-               <h3>Information Protection</h3>
-               <p>We take all important measures to shield customers' private information from security threats.</p>
-               <h3>Publication of Information to Third Parties</h3>
-               <p> Users' non-public data will not be disclosed to third parties with out their consent, besides as required through law.</p>
-               <h3>Policy Updates</h3>
-               <p>We regularly update our privacy policy to comply with modifications in the law and our operations.
+               <h3>{t('PrivacyPolicy.title3')}</h3>
+               <p>{t('PrivacyPolicy.description3')}</p>
+               <h3>{t('PrivacyPolicy.title4')}</h3>
+               <p> {t('PrivacyPolicy.description4')}</p>
+               <h3>{t('PrivacyPolicy.title5')}</h3>
+               <p>{t('PrivacyPolicy.description5')}
                </p>
                </div>
           </section>
