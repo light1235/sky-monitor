effa18e55a92e4549c906e648f34f14175667c19
Lighty <shliakhov.frontend@gmail.com>
2024-09-06 23:03:27 +0300

    add translate for page pop up and pin

--- src/app/[locale]/listing-pin/page.js
+++ src/app/[locale]/listing-pin/page.js
@@ -2,61 +2,51 @@ import React from 'react';
 import './index.scss'
 import ListingImage from '/src/assets/main/images/listing-service.jpg'
 import Image from "next/image";
+import {useTranslations} from "next-intl";
 
 const ListingPinPackage = () => {
+     const t = useTranslations();
      return (
           <div className="listing-service__page">
                <div className="page-container">
-                    <h1>Pin service includes raising and fixing your project listing to the first position in the
-                         Overview section</h1>
-                    <h2>How does it work? </h2>
-                    <p>Gradually with the release of new projects the original ones will move
-                         according to the order displacing each other, your project, which was displaced by others will
-                         again be on the first places and will be fixed on the first position that will improve the
-                         visibility, reputation and rating of your project.</p>
+                    <h1>{t('ListingPin.heading1')}</h1>
+                    <h2>{t('ListingPin.heading2')}</h2>
+                    <p>{t('ListingPin.paragraph1')}</p>
                     <Image src={ListingImage} alt={'example image'}/>
-                    <h3>Features of the service (Subtleties)</h3>
+                    <h3>{t('ListingPin.heading3')}</h3>
                     <ul>
-                         <li><p>The project will be raised and locked in the Overview section.</p></li>
-                         <li><p>The project is not moved by new released projects</p></li>
-                         <li><p>Project after the end of the period of fixing time (Term which you choose when
-                              activating this service) begins to move down when new projects are released. </p></li>
-                         <li><p>The project will be moved down one unit if other projects use the same service. </p>
-                         </li>
+                         <li><p>{t('ListingPin.feature1')}</p></li>
+                         <li><p>{t('ListingPin.feature2')}</p></li>
+                         <li><p>{t('ListingPin.feature3')}</p></li>
+                         <li><p>{t('ListingPin.feature4')}</p></li>
                     </ul>
-                    <h3>Advantages</h3>
-                    <ul>
-                         <li><p><strong>Increased visibility:</strong> Your project will become more visible in this
-                              segment, which will attract more attention from potential investors and users.</p></li>
-                         <li><p><strong>Improved reputation:</strong> Since your project will be at the top of the list,
-                              people will first of all participate in your project and leave positive votes that will
-                              create a certain rating for your project that will increase trust and loyalty.</p></li>
-                         <li><p><strong>Increased investment:</strong> As the visibility and reputation of a project
-                              improves, the likelihood of attracting new investors interested in supporting a successful
-                              and promising project increases. </p></li>
-                         <li><p>Fixing your project in the top of the section-which will bring constant, additional
-                              attention of the audience to your project as well as increase investment in it. </p>
-                         </li>
-                    </ul>
-                    <h3>"As a result, utilizing our service to elevate and anchor up your Pin project will lead to its
-                         significant growth and development, increased customer base and investment appeal."</h3>
-                    <h3> Follow the simple steps to purchase the Pin service: </h3>
-                    <ul>
-                         <li><p>Click at the top of the website + Add program </p></li>
-                         <li><p>Select Pin&Raise and press continue</p></li>
-                         <li><p>A pop-up window will appear ( Fill it in step by step)</p>
-                         </li>
-                    </ul>
-                    <h3>Form filling:</h3>
-                    <ul>
-                         <li><p>Specify your company name “Example - Skorpions ”</p></li>
-                         <li><p>In the form, the “Your listing” field, the project that has already been placed by you on the monitor will be pulled up automatically.</p></li>
-                         <li><p>In the “Service” section, select the “Pin” service.</p> </li>
-                              <li><p>Select the validity period.</p></li>
-                              <li><p>Click continue and pay.</p></li>
-                              <li><p>Specify the payment method that suits you.</p></li>
-                              <li><p>Pay for this service, after checking your project will be raised and fixed on the first place in the section corresponding to your project.</p></li>
 
+                    <h3>{t('ListingPin.heading4')}</h3>
+                    <ul>
+                         <li><p><strong>{t('ListingPin.advantage1')}</strong></p></li>
+                         <li><p><strong>{t('ListingPin.advantage2')}</strong></p></li>
+                         <li><p><strong>{t('ListingPin.advantage3')}</strong></p></li>
+                         <li><p>{t('ListingPin.advantage4')}</p></li>
+                    </ul>
+
+                    <h3>{t('ListingPin.result')}</h3>
+
+                    <h3>{t('ListingPin.heading5')}</h3>
+                    <ul>
+                         <li><p>{t('ListingPin.step1')}</p></li>
+                         <li><p>{t('ListingPin.step2')}</p></li>
+                         <li><p>{t('ListingPin.step3')}</p></li>
+                    </ul>
+
+                    <h3>{t('ListingPin.heading6')}</h3>
+                    <ul>
+                         <li><p>{t('ListingPin.form1')}</p></li>
+                         <li><p>{t('ListingPin.form2')}</p></li>
+                         <li><p>{t('ListingPin.form3')}</p></li>
+                         <li><p>{t('ListingPin.form4')}</p></li>
+                         <li><p>{t('ListingPin.form5')}</p></li>
+                         <li><p>{t('ListingPin.form6')}</p></li>
+                         <li><p>{t('ListingPin.form7')}</p></li>
                     </ul>
                </div>
           </div>
--- src/app/[locale]/pop-up-advertising/page.js
+++ src/app/[locale]/pop-up-advertising/page.js
@@ -2,36 +2,36 @@ import React from 'react';
 import './index.scss'
 import PopUpImage from '@/assets/main/images/pop-up-service.jpg'
 import Image from "next/image";
+import {useTranslations} from "next-intl";
 
 const PopUpPackages  = () => {
+     const t = useTranslations();
      return (
           <div className="pop-up-service__page">
                <div className="page-container">
-                    <h1><strong>Popup banner </strong></h1>
-                    <h3>Is a popup on a website that shows important information, offer or promotion to grab the
-                         attention of users.</h3>
+                    <h1><strong>{t('PopupBanner.heading')}</strong></h1>
+                    <h3>{t('PopupBanner.subHeading')}</h3>
 
                     <Image src={PopUpImage} alt={'example image'}/>
-                    <p>Shows ads once to the user who visited the site, when you log in again will not be shown.
-                         The advantage of this advertisement is that it will be seen by all users who visit the
-                         site</p>
-                    <h3>What you need to do to order this service:</h3>
+                    <p>{t('PopupBanner.popupDescription')}</p>
+
+                    <h3>{t('PopupBanner.whatToDoTitle')}</h3>
                     <ul>
-                         <li><p>On the main menu at the very top of the site find and click on add program</p></li>
-                         <li><p>In the Pop-up window will appear 3 options to add ads, choose (Pop-up)</p></li>
-                         <li><p>Fill in the pop-up window with the specified parameters from the beginning and in
-                              order </p></li>
+                         <li><p>{t('PopupBanner.steps.step1')}</p></li>
+                         <li><p>{t('PopupBanner.steps.step2')}</p></li>
+                         <li><p>{t('PopupBanner.steps.step3')}</p></li>
                     </ul>
-                    <h3>Form filling:</h3>
+
+                    <h3>{t('PopupBanner.formFillingTitle')}</h3>
                     <ul>
-                         <li><p>Specify the name of your site “Example - skorpions”</p></li>
-                         <li><p>Specify a description of what the user should see in the pop-up ad “Example”-Promising project that has been on the market for more than 5 years Hamster Kombat</p></li>
-                         <li><p>Specify Site Url -this is the address of your site “Example -https://www.skorpions.com/</p></li>
-                         <li><p>Click on + and add a picture for your Pop-up in size 123x123 in format (jpg,png,gif)</p></li>
-                         <li><p>Enter your valid email in the field “Example - skorpions@gmail.com”</p></li>
-                         <li><p>Select the period of validity of the Pop-ap </p></li>
-                         <li><p>Specify the payment method that suits you and click on the button “Proceed to payment”.</p></li>
-                         <li><p>Pay for this service, after moderation your Pop-up will be advertised to users of the project. </p></li>
+                         <li><p>{t('PopupBanner.formFilling.step1')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step2')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step3')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step4')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step5')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step6')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step7')}</p></li>
+                         <li><p>{t('PopupBanner.formFilling.step8')}</p></li>
                     </ul>
                </div>
           </div>
