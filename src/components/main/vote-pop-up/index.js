eede9839eaab0a4f50702a2fdd5cf07cd49b1183
Lighty <shliakhov.frontend@gmail.com>
2024-09-06 22:24:32 +0300

    add page banner and form translate

--- src/components/main/vote-pop-up/index.js
+++ src/components/main/vote-pop-up/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.scss'
-
+import { useLocale } from 'next-intl';
 const VotePopUp = ({name,close,voteData,disable}) => {
      // console.log(close);
-
+     const locale = useLocale();
      return (
           <div className="vote-pop-up">
                <div className="item-top">
                     <div className="item-top-inner">
-                         <p>You can vote only once a week</p>
+                         <p>{locale === 'en' ? 'You can vote only once a week':'Голосовать можно раз в неделю'}</p>
                          <div className="close-button" onClick={close}>
                               <div className="top-stick"></div>
                               <div className="bottom-stick"></div>
@@ -16,11 +16,14 @@ const VotePopUp = ({name,close,voteData,disable}) => {
                     </div>
                </div>
                <div className="item-main-content">
-                    <p>Are you sure want to vote {voteData === '-1' ? 'against' : 'for'} <strong>{name}</strong> ?</p>
+                    <p>{locale === 'en'? 'Are you sure want to vote':'Хотите проголосовать'}  {voteData === '-1'
+                         ? (locale === 'en' ? 'against' : 'против')
+                         : (locale === 'en' ? 'for' : 'за')
+                    } <strong>{name}</strong> ?</p>
                </div>
                <div className="item-buttons" >
-                    <div className="button-yes" onClick={disable}>yes</div>
-                    <div className="button-no" onClick={close}>no</div>
+                    <div className="button-yes" onClick={disable}>{locale === 'en'? 'yes':'да'}</div>
+                    <div className="button-no" onClick={close}>{locale === 'en'? 'no':'нет'}</div>
                </div>
           </div>
      );
